e106a023ef86e4370847ecc21e893521e429aff5
Uriel Aguilar <uaguilardeveloper@gmail.com>
2022-04-05 10:19:19 -0500

    Se añade el arithetical

---
+++ actividad2/arithmetic-lexer.rkt
@@ -0,0 +1,18 @@
+#lang racket
+
+#|
+Alvaro Garcia
+Uriel Aguilar
+
+2022-03-18
+
+Simple implementation of a Deterministic Finite Automaton
+Identify all the token types in an arithmetical expresion string
+
+Part of this code Gilberto Echeverria.
+|#
+
+
+(define (arithmetical-lexer input)
+
+)
