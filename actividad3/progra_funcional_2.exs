e42a52298cd5833a7be9bc6359fb915cf2578cf1
Alvaro <ag587635@gmail.com>
2022-05-12 22:16:18 -0500

    Ejercicios 3 y 10

--- actividad3/progra_funcional_2.exs
+++ actividad3/progra_funcional_2.exs
@@ -1,8 +1,10 @@
 # Uriel Aguilar
 # Alvaro Garcia
-# Actividad: Programación funcional parte 2
+# Activity: Functional programming 2
 
 defmodule Sorts do
+
+
     @doc """
         Insert n in a list
     """
@@ -29,6 +31,8 @@ defmodule Sorts do
         do: do_insert(tail, n, [head | new_list], true)
 
 
+
+
     @doc """ 
     Insertion sort 
     """
@@ -41,6 +45,69 @@ defmodule Sorts do
     defp do_sort([head | tail], new_list), 
         do: do_sort(tail, insert(new_list, head))
 
+
+
+
+    @doc """
+    Function that rotates list n times
+    """
+    def rotate_left(list, n) do
+        cond do
+        #n is positive or zero case
+        n >= 0 ->
+        cond do
+            #if conditions are met, return list
+            n == length(list) || n == 0 || length(list) == 0 ->
+            list
+            #if n is smaller than the list's length, slice list and sort
+            n < length(list) ->
+            #_a is for avoiding warning
+            _a = Enum.slice(list, n..length(list)-1) ++ Enum.slice(list, 0..n-1)
+            #if n is bigger than the list's length, obtain the remainder, slice list and sort
+            n > length(list) ->
+            #_a is for avoiding warning
+            _a = Enum.slice(list, (rem(n, length(list)))..length(list)-1) ++ Enum.slice(list, 0..(rem(n, length(list)))-1)
+        end
+        #n is negative or zero case
+        n<0 ->
+
+            cond do
+            #if conditions are met, return list
+            (n* -1) == length(list) || length(list) == 0 ->
+                list
+            #if n is smaller than the list's length, slice list and sort. Sort in the other direction
+            (n* -1) < length(list) ->
+                #_a is for avoiding warning
+                _a =  Enum.slice(list, n..length(list)-1) ++ Enum.slice(list, 0..n-1)
+            #if n is bigger than the list's length, obtain the remainder, slice list and sort. Sort in the other direction
+            (n* -1) > length(list) ->
+            #_a is for avoiding warning
+            _a =  Enum.slice(list, (rem((n), length(list))), length(list)-1) ++ Enum.slice(list, 0..(rem((n), length(list)))-1)
+
+            end
+        end
+    end
+
+
+    
+
+    @doc """
+    Encode function adapted from  class exercise
+    """
+    def encode(list), do: do_encode(list, [], [])
+    defp do_encode([], _temp, result),
+        do: Enum.reverse(result)
+    # The list has only one element
+    defp do_encode([head | []], temp, result), 
+        #The size of the temporary list plus one and the value are inserted to result 
+        do: do_encode([], [], [{(length(temp)+1), head } | result])
+    # The first two elements are equal
+    defp do_encode([head, head | tail], temp, result),
+        do: do_encode([head | tail], [head | temp], result)
+    # The first two elements are different
+    defp do_encode([head | tail], temp, result),
+      #The size of the temporary list plus one and the value are inserted to result 
+        do: do_encode(tail, [], [{(length(temp)+1), head } | result])
  
 
 end
