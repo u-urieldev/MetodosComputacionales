20519908691ac78d8a54f06ac159693b455542b6
Uriel Aguilar <uaguilardeveloper@gmail.com>
2022-05-20 10:40:05 -0500

    Stable

--- evidencia1/evidencia2.exs
+++ evidencia1/evidencia2.exs
@@ -11,19 +11,19 @@ defmodule Evidencia do
       temp1 = File.read!(in_filename)
       # Match keys
       keys1 = Regex.replace(~r/"(\s*[^" [])*"(?=\s*:)|(?!)/,temp1, "<span class='object-key'>\\g{0}</span>") 
-      # Match puntuation
-      keys2 = Regex.replace(~r/\{|\}|:|\[|\]|,(?!.*")/,keys1, "<span class='puntuation'>\\g{0}</span>")
+      # Match strings
+      keys2 = Regex.replace(~r/"(\s*[^" [>])*"(?!<)|(?!)/,keys1, "<span class='string'>\\g{0}</span>")
       # Match Numbers
       keys3 = Regex.replace(~r/-?\d+\.?\d*([eE]?[-\+]?\d+)?(?!.*")/,keys2, "<span class='number'>\\g{0}</span>")
       # Match Booleans
       keys4 = Regex.replace(~r/(true|false)(?!.*")/,keys3, "<span class='bool'>\\g{0}</span>")
       # Match Null
       keys5 = Regex.replace(~r/null(?!.*")/,keys4, "<span class='null'>\\g{0}</span>")
-      # Match strings
-      #keys3 = Regex.replace(~r/[^>]"(\s*[^"])*"|(?!)/,keys2, "<span class='string'>\\g{0}</span>")
+      # Match Puntuation
+      keys6 = Regex.replace(~r/(:|,|\[|\]|\{|\})(?![^<]*>|[^<>]*<\/)/,keys5, "<span class='puntuation'>\\g{0}</span>")
       
 
-      File.write(out_filename, keys5)
+      File.write(out_filename, keys6)
             
     end
 end
