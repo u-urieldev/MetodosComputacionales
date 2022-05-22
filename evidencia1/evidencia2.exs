0ce4adc3f7996a00e07c53ea7092e7ef7424294e
Uriel Aguilar <uaguilardeveloper@gmail.com>
2022-05-22 00:04:51 -0500

    SStable (archivo de prueba 2 por arreglar)

--- evidencia1/evidencia2.exs
+++ evidencia1/evidencia2.exs
@@ -1,31 +1,42 @@
 # Alvaro
-# Uriel
+# Uriel A01781698
+# Actividad Integradora 3.4: Syntax highlighter.
 
-defmodule Evidencia do
-    
+defmodule Parce do
     @doc """
-      Parce a JSON file given and create and html file whit
-      token hightlight syntaxis.
+      Parce a JSON file given and create and 
+      html file whit token hightlight syntaxis.
     """
-    def parceJSON(in_filename, out_filename) do
-      temp1 = File.read!(in_filename)
-      # Match keys
-      keys1 = Regex.replace(~r/"(\s*[^" [])*"(?=\s*:)|(?!)/,temp1, "<span class='object-key'>\\g{0}</span>") 
-      # Match strings
-      keys2 = Regex.replace(~r/"(\s*[^" [>])*"(?!<)|(?!)/,keys1, "<span class='string'>\\g{0}</span>")
-      # Match Numbers
-      keys3 = Regex.replace(~r/-?\d+\.?\d*([eE]?[-\+]?\d+)?(?!.*")/,keys2, "<span class='number'>\\g{0}</span>")
-      # Match Booleans
-      keys4 = Regex.replace(~r/(true|false)(?!.*")/,keys3, "<span class='bool'>\\g{0}</span>")
-      # Match Null
-      keys5 = Regex.replace(~r/null(?!.*")/,keys4, "<span class='null'>\\g{0}</span>")
-      # Match Puntuation
-      keys6 = Regex.replace(~r/(:|,|\[|\]|\{|\})(?![^<]*>|[^<>]*<\/)/,keys5, "<span class='puntuation'>\\g{0}</span>")
+    def json(in_filename, out_filename) do
+      new_text = 
+        in_filename
+        |> File.read!()
+        # Match keys
+        |> tag_replace(~r/"(\s*[^" [])*"(?=\s*:)|(?!)/,
+         "<span class='object-key'>\\g{0}</span>") 
+        # Match strings
+        |> tag_replace(~r/"(\s*[^" [>])*"(?!<)|(?!)/,
+         "<span class='string'>\\g{0}</span>") 
+        # Match numbers
+        |> tag_replace(~r/-?\d+\.?\d*([eE]?[-\+]?\d+)?(?!.*")/,
+         "<span class='number'>\\g{0}</span>") 
+        # Match reserved words
+        |> tag_replace(~r/(true|false|null)(?!.*")/, 
+        "<span class='reserved-word'>\\g{0}</span>")  
+        # Match puntuation
+        |> tag_replace(~r/(:|,|\[|\]|\{|\})(?![^<]*>|[^<>]*<\/)/, 
+        "<span class='puntuation'>\\g{0}</span>") 
       
-
-      File.write(out_filename, keys6)
-            
+      createFile(out_filename, new_text)
     end
+
+    defp tag_replace(file, regExp, tag), do: 
+      Regex.replace(regExp, file, tag)
+
+    defp createFile(out_file, body), do:
+      File.write(out_file, 
+        File.read!("header.html") <> body <> File.read!("footer.html"))
+    
 end
 
-Evidencia.parceJSON("Test_files/example_1.json", "new.html")
+Parce.json("Test_files/example_1.json", "new.html")
--- evidencia1/regex_patterns.md
+++ evidencia1/regex_patterns.md
@@ -1,30 +1,19 @@
 # Regex Patterns for JSON
 
-Agregamos el lookahead negativo <code>(?!.*")</code> para matchear
-lo que este afuera de las comillas.
+We add the negative lookahead <code>(?!.*")</code> to match 
+expresions out of quotes.
 
 * **Keys:** 
     > <p> "(\s*[^" [])*"(?=\s*:)|(?!) </p>
 
 * **Puntuation:**
-    > <p> \{|\}|:|\[|\]|,(?!.*") </p>
-    > <p> [:](?!.*")</p>
-    (:)(?![^<]*>|[^<>]*</)
+    > <p> (:|,|\[|\]|\{|\})(?![^<]*>|[^<>]*<\/) </p>
 
 * **Numbers:** 
     > <p> -?\d+\.?\d*([eE]?[-\+]?\d+)?(?!.*") </p>
 
-* **Boolean:**
-    > <p> (true|false)(?!.*") </p>
+* **Reserved Words:**
+    > <p> (true|false|null)(?!.*") </p>
 
-* **Null:**
-    > <p> null(?!.*") </p>
-
-* **String: (por decidir)**
-    > <p> "(\s*\w+[^"])*"(?!\s*:)|(?!) </p>
-    > <p> -> "(\s*[^" [])*"(?!\s*:)|(?!) </p> 
-    > <p> --> [^>]"(\w+\s*[^"])*"|(?!) </p>
-    > <p> ---->[^>]"(\s*[^"])*"|(?!) </p>
-    > <p> ---->[^>]"(\s*[^"'])*"|(?!)</p>
-    > <p> "(\s*[^" [])*"(?!<)|(?!) </p>
-    > <p>  "(\s*[^" [<])*"(?!<)|(?!) </p>
+* **String:**
+    > <p>  (:|,|\[|\]|\{|\})(?![^<]*>|[^<>]*<\/) </p>
