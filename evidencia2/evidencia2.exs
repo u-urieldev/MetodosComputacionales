d252bbd89f05e3f9b53c9df500c88ca06028aba1
Uriel Aguilar <uaguilardeveloper@gmail.com>
2022-06-09 16:09:52 -0500

    Se añade funcion en secuencial

--- evidencia2/evidencia2.exs
+++ evidencia2/evidencia2.exs
@@ -37,19 +37,33 @@ defmodule Parce do
       File.write(out_file, 
         File.read!("sheme/header.html") <> body <> File.read!("sheme/footer.html"))
 
-    
+
+    @doc """
+      Parce multiple json files in secuencial.
+      Funcion recive a list with input files 
+      and their corresponding output file.  
+      Or a list of tuples in form of pairs {input_file, out_putfile}.
+    """
+    def multi_json(files), do: 
+      Enum.map(files, fn par -> json(elem(par,0), elem(par,1)) end )
+
+    def multi_json(infiles, outfiles), do:
+      List.zip([infiles, outfiles])
+      |> Enum.map(fn par -> json(elem(par,0), elem(par,1)) end )
+
+
     @doc """
       Parce multiple json files in parall.
       Funcion recive a list with input files 
       and their corresponding output file.  
       Or a list of tuples in form of pairs {input_file, out_putfile}.
     """
-    def multi_json(infiles, outfiles) do
+    def multi_json_parall(infiles, outfiles) do
       List.zip([infiles, outfiles])
       |> Enum.map(&Task.start(fn -> json(elem(&1, 0), elem(&1, 1)) end ))
     end
 
-    def multi_json(files) do
+    def multi_json_parall(files) do
       files
       |> Enum.map(&Task.start(fn -> json(elem(&1, 0), elem(&1, 1)) end ))
     end
@@ -59,11 +73,14 @@ end
 
 #Parce.json("Test_files/example_1.json", "new.html")
 
-infile = ["Test_files/example_0.json", "Test_files/example_1.json", "Test_files/example_2.json"]
-outfile = ["Out_files/new0.html", "Out_files/new1.html","Out_files/new2.html"]
+# infile = ["Test_files/example_0.json", "Test_files/example_1.json", "Test_files/example_2.json"]
+# outfile = ["Out_files/new0.html", "Out_files/new1.html","Out_files/new2.html"]
 # files = [{"Test_files/example_0.json", "Out_files/new0.html"},
 #          {"Test_files/example_1.json", "Out_files/new1.html"},
 #          {"Test_files/example_2.json", "Out_files/new2.html"}]
 
-Parce.multi_json(infile, outfile)
+# Parce.multi_json(infile, outfile)
 # Parce.multi_json(files)
+
+#Parce.multi_json_parall(infile, outfile)
+#Parce.multi_json_parall(files)
